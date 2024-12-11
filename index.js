b66ca16c52d57e1fc804b511632acabcae18f34e
John-Rashta <john_rashta@outlook.com>
2024-12-11 15:59:13 +0000

    feat: change to express

--- index.js
+++ index.js
@@ -1,37 +1,42 @@
 const http = require('node:http');
 const fs = require('fs');
+const express = require("express");
+const app = express();
 
+const PORT = 8080;
 
-const server = http.createServer((req, res) => {
-    const newURL = req.url.slice(1);
-    if (newURL === "") {
-        fs.readFile("./src/index.html", (err, data) => {
-            if (err) {
-                fs.readFile("./src/404.html", (err, data) => {
-                    res.writeHead(404, { 'Content-Type': 'text/html' });
-                    res.write(data);
-                    return res.end();
-                });
-            } else {
-                res.writeHead(200, { 'Content-Type': 'text/html' });
+app.get("/", (req, res) => {
+    fs.readFile("./src/index.html", (err, data) => {
+        if (err) {
+            fs.readFile("./src/404.html", (err, data) => {
+                res.writeHead(404, { 'Content-Type': 'text/html' });
                 res.write(data);
                 return res.end();
-            }
-        })  
-    } else {
-            fs.readFile(`./src/${newURL}.html`, {flag: "r"}, (err, data) => {
+            });
+        } else {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(data);
+            return res.end();
+        }
+    })  
+});
+
+app.get("/:name", (req, res) => {
+    const newURL = req.url.slice(1);
+    fs.readFile(`./src/${newURL}.html`, {flag: "r"}, (err, data) => {
                 
-                if (err) {
-                    fs.readFile("./src/404.html", (err, data) => {
-                        res.writeHead(404, { 'Content-Type': 'text/html' });
-                        res.write(data);
-                        return res.end();
-                    });
-                } else {
-                    res.writeHead(200, { 'Content-Type': 'text/html' });
-                    res.write(data);
-                    return res.end();
-                }
-            })
-    } 
-}).listen(8080);
+        if (err) {
+            fs.readFile("./src/404.html", (err, data) => {
+                res.writeHead(404, { 'Content-Type': 'text/html' });
+                res.write(data);
+                return res.end();
+            });
+        } else {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(data);
+            return res.end();
+        }
+    })
+});
+
+app.listen(PORT);
